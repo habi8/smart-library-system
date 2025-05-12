073bd91a4e1946005a52cfe35ffb09ce39fefc9b
habi8 <bsse1422@iit.du.ac.bd>
2025-05-12 21:14:52 +0600

    started userController

---
+++ Phase_2/UserService/controllers/userController.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+// User Schema
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  role: { type: String, enum: ['student', 'librarian'], required: true },
+});
+const User = mongoose.model('User', userSchema);
