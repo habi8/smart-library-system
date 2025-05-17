7f7739af84eda201cbf5ac0840584f7751900cc4
habi8 <bsse1422@iit.du.ac.bd>
2025-05-17 22:24:05 +0600

    loan service completed

--- Phase_2/LoanService/server.js
+++ Phase_2/LoanService/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 //const dotenv = require('dotenv');
-//const userRoutes = require('./routes/users');
+const loanRoutes = require('./routes/loanRoute');
 
 const app = express();
 const connect = mongoose.connect('mongodb://127.0.0.1:27017/LoanDB');
@@ -12,6 +12,7 @@ connect.then(()=>{
 .catch((err)=>{
     console.log("Not connected",err);
 })
-
+app.use(express.json());
+app.use('/api/loans', loanRoutes);
 const PORT =  3003;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
