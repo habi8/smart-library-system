1241a3b8a8db1124030bf82378fa1d3183b8527b
habi8 <bsse1422@iit.du.ac.bd>
2025-05-17 20:36:50 +0600

    user service endpoint complete

--- Phase_2/UserService/routes/userRoute.js
+++ Phase_2/UserService/routes/userRoute.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const c = require('../controllers/userController');
+const {
+  createUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} = require('../controllers/userController');
 
-router.post('/',c.createUser)
+router.post('/', createUser);
+router.get('/', getAllUsers);
+router.get('/:id', getUser);
+router.put('/:id', updateUser);
+
 
 module.exports = router;
